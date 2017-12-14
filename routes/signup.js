f54b8a16b9624473de85f5ec59b8136108dba58e
mohan <mohankukreja1@gmail.com>
2017-12-14 19:26:40 +0530

    nineth commit
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- routes/signup.js
+++ routes/signup.js
@@ -1,7 +1,26 @@
 const route=require('express').Router()
+var passport = require('passport');
+var expressValidator = require('express-validator');
+
+var LocalStrategy = require('passport-local').Strategy;
+
 //const db=require('../models/db')
 
+route.post('/',(req,res)=>{
+    var name = req.body.name;
+    var email = req.body.email;
+    var username = req.body.username;
+    var password = req.body.password;
+    var password2 = req.body.passwordRepeat;
+    var temp='password not matching'
 
+    if(password!=password2){
+    res.redirect('/signup.html')
+    }
+    else{
+
+    }
+})
 
 exports=module.exports={
     route
